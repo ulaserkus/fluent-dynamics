ab1d60397e3a4181779c802cfac6ba6cf88de613
Ulaş ERKUŞ <ulas.erkus@caretta.net>
2025-08-19 14:48:03 +0300

    Add FluentDynamics.Samples.NetFramework project
    Updated project files to include metadata, configuration, and dependencies for the new FluentDynamics.Samples.NetFramework project. Implemented a console application for querying Microsoft Dynamics 365. Modified solution structure to integrate the new project and ensure proper build configurations.

---
+++ FluentDynamics.Samples.NetFramework/Program.cs
@@ -0,0 +1,31 @@
+﻿using FluentDynamics.QueryBuilder;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+
+namespace FluentDynamics.Samples.NetFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string clientId = "<client-id>";
+            string clientSecret = "<client-secret>";
+            Uri serviceUri = new Uri("<org-url>");
+            string connectionString = $"AuthType=ClientSecret;Url={serviceUri};ClientId={clientId};ClientSecret={clientSecret};";
+            IOrganizationService service = new CrmServiceClient(connectionString);
+
+            var results = Query.For("account")
+                .Select("name", "accountnumber", "telephone1")
+                .Where("statecode", ConditionOperator.Equal, 0)
+                .OrderBy("name")
+                .Top(10)
+                .RetrieveMultiple(service)
+                .ToList();
+
+
+            Console.WriteLine("EOP");
+        }
+    }
+}
