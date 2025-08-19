e31d733d40230bfc8f241428453f3966911cf9dd
Ulaş ERKUŞ <ulas.erkus@caretta.net>
2025-08-19 17:59:02 +0300

    Remove example usage comments from Query.cs
    Deleted code comments that provided an example usage of the Dynamics 365/Dataverse fluent query API, including the summary and code snippet for creating a query and retrieving results.

--- FluentDynamics.QueryBuilder/Query.cs
+++ FluentDynamics.QueryBuilder/Query.cs
@@ -4,17 +4,6 @@
     /// Entry point for the Dynamics 365/Dataverse fluent query API.
     /// Provides factory methods to create query builders for various entity types.
     /// </summary>
-    /// <example>
-    /// <code>
-    /// // Example usage:
-    /// var query = Query.For("account")
-    ///     .Select("name", "accountnumber")
-    ///     .Where("statecode", ConditionOperator.Equal, 0)
-    ///     .OrderBy("name");
-    /// 
-    /// var results = query.RetrieveMultiple(organizationService);
-    /// </code>
-    /// </example>
     public static class Query
     {
         /// <summary>
